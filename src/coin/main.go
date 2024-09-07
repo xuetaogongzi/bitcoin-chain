2996a081b3217244a7d8e498be12952bbae69567
bym <bym@bymdeMacBook-Pro.local>
2024-09-07 20:12:16 +0800

    part2

--- src/coin/main.go
+++ src/coin/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bitcoin-chain/src/core"
 	"fmt"
+	"strconv"
 )
 
 func main() {
@@ -17,5 +18,10 @@ func main() {
 		fmt.Printf("Prev. hash: %x\n", block.PrevBlockHash)
 		fmt.Printf("Data: %s\n", block.Data)
 		fmt.Printf("Hash: %x\n", block.Hash)
+		fmt.Printf("Nonce: %d\n", block.Nonce)
+
+		pow := core.NewProofOfWork(block)
+		fmt.Printf("Pow: %s\n", strconv.FormatBool(pow.Validate()))
+		fmt.Println()
 	}
 }
